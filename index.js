db26ac4b164e17e4e71e4bd86284a485af83fd87
FavioA <134189396+FavioA@users.noreply.github.com>
2023-11-13 00:03:51 -0800

    Updated code in adding an employee to query the db, result.map to transform the response, and passed in the date to inquirer.prompt.

--- index.js
+++ index.js
@@ -71,154 +71,173 @@ function init() {
         });
       } else if (response.choice === "Add a department") {
         inquirer
-        .prompt ([
+          .prompt([
             {
-            message: "What is the name of the department that you'd like to add?",
-            type: "input",
-            name: 'departmentName'
-            }
-        ]) .then (data=> {
+              message:
+                "What is the name of the department that you'd like to add?",
+              type: "input",
+              name: "departmentName",
+            },
+          ])
+          .then((data) => {
             const sql = `INSERT INTO department (name) VALUES (?)`;
-        const params = [data.departmentName];
-        db.query(sql, params, (err, result) => {
-            if (err) {
-              console.log(err);
-              return;
-            } else {
-              console.table(result);
-              init();
-            }
+            const params = [data.departmentName];
+            db.query(sql, params, (err, result) => {
+              if (err) {
+                console.log(err);
+                return;
+              } else {
+                console.table(result);
+                init();
+              }
+            });
           });
-        });
-           
-    } else if (response.choice === "Add a role") {
+      } else if (response.choice === "Add a role") {
         inquirer
-        .prompt ([
+          .prompt([
             {
-            message: "What is the name of the role that you would like to add?",
-            type: "input",
-            name: 'title'
+              message:
+                "What is the name of the role that you would like to add?",
+              type: "input",
+              name: "title",
             },
             {
-            message: "What is the salary of the role that you would like to add?",
-            type: "input",
-            name: 'salary'
+              message:
+                "What is the salary of the role that you would like to add?",
+              type: "input",
+              name: "salary",
             },
             {
-            message: "What department does this role belong to?",
-            type: "list",
-            choices: [1, 2],
-            name: 'departmentId',
-            }
-        ]) .then (data=> {
+              message: "What department does this role belong to?",
+              type: "list",
+              choices: [1, 2],
+              name: "departmentId",
+            },
+          ])
+          .then((data) => {
             const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
             const params = [data.title, data.salary, data.departmentId];
             db.query(sql, params, (err, result) => {
-            if (err) {
-              console.log(err);
-              return;
-            } else {
-            console.table(result);
-            init();
-            }
+              if (err) {
+                console.log(err);
+                return;
+              } else {
+                console.table(result);
+                init();
+              }
+            });
           });
-        })
       } else if (response.choice === "Add an employee") {
-        inquirer.prompt ([
-            {
-            message: "What is the new employee's first name?",
-            type: "input",
-            name: 'firstName'
-            },
-            {
-            message: "What is the new employee's last name?",
-            type: "input",
-            name: 'lastName'
-            },
-            {
-            message: "What is the new employee's role?",
-            type: "list",
-            choices: 
-            [
-                'HR Rep',
-                'Technologist',
-                'Employee Relations Manager',
-                'Recruiter',
-                'Web Developer',
-                'Application Analyst',
-                'User Interface Designer'
-            ],
-            name: 'title'
-            },
-            {
-            message: "Who is the manager of the employee?",
-            type: "list",
-            choices: [1],
-            name: 'managerId'
-            },
-        ]) .then (data=> {
-            const sql = `INSERT INTO employee (first_name, last_name, title, manager_id) VALUES (?, ?, ?, ?)`;
-        const params = [data.firstName, data.lastName, data.title, data.managerId];
-        db.query(sql, params, (err, result) => {
-            if (err) {
-              console.log(err);
-              return;
-            } else {
-              console.table(result);
-              init();
-            }
-          });
-        })
+        const sql = `SELECT * FROM role`;
+        db.query(sql, (err, result) => {
+          if (err) {
+            console.log(err);
+            return;
+          } else {
+            const mappedData = result.map(({ title, id }) => ({
+              name: title,
+              value: id,
+            }));
+            inquirer
+              .prompt([
+                {
+                  message: "What is the new employee's first name?",
+                  type: "input",
+                  name: "firstName",
+                },
+                {
+                  message: "What is the new employee's last name?",
+                  type: "input",
+                  name: "lastName",
+                },
+                {
+                  message: "What is the new employee's role_id?",
+                  type: "list",
+                  choices: mappedData,
+                  name: "roleId",
+                },
+                {
+                  message: "Who is the manager of the employee?",
+                  type: "list",
+                  choices: [1],
+                  name: "managerId",
+                },
+              ])
+              .then((data) => {
+                const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+                const params = [
+                  data.firstName,
+                  data.lastName,
+                  data.roleId,
+                  data.managerId,
+                ];
+                db.query(sql, params, (err, result) => {
+                  if (err) {
+                    console.log(err);
+                    return;
+                  } else {
+                    console.table(result);
+                    init();
+                  }
+                });
+              });
+          }
+        });
       } else if (response.choice === "Update an employee role") {
         inquirer
-        .prompt ([
+          .prompt([
             {
-                message: "Which employee's role would you like to update?",
-                type: "list",
-                choices: [
-                    'Louis Armstrong', 
-                    'Jimi Hendrix', 
-                    'Charlie Parker', 
-                    'Billie Holiday', 
-                    'Hector Lavoe', 
-                    'Celia Cruz', 
-                    'Dinah Washington'
-                ],
-                name: 'currentTitle',
+              message: "Which employee's role would you like to update?",
+              type: "list",
+              choices: [
+                "Louis Armstrong",
+                "Jimi Hendrix",
+                "Charlie Parker",
+                "Billie Holiday",
+                "Hector Lavoe",
+                "Celia Cruz",
+                "Dinah Washington",
+              ],
+              name: "currentTitle",
             },
             {
-                message: "Which role would you like to assign to this employee?",
-                type: "list",
-                choices: [ 
-                    'HR Rep',
-                    'Technologist',
-                    'Employee Relations Manager',  
-                    'Recruiter',  
-                    'Web Developer', 
-                    'Application Analyst', 
-                    'User Interface Designer'
-                ],
-                name: 'newTitle',
+              message: "Which role would you like to assign to this employee?",
+              type: "list",
+              choices: [
+                "HR Rep",
+                "Technologist",
+                "Employee Relations Manager",
+                "Recruiter",
+                "Web Developer",
+                "Application Analyst",
+                "User Interface Designer",
+              ],
+              name: "newTitle",
             },
-        
-        ])  .then (data=> {
+          ])
+          .then((data) => {
             const sql = `UPDATE role SET title = ?, salary = ?, department_id = ? WHERE id = ?`;
-        const params = [data.currentTitle, data.newTitle, data.salary, data.departmentId, data.id];
-        db.query(sql, params, (err, result) => {
-            if (err) {
-              console.log(err);
-              return;
-            } else {
-              console.table(result);
-              init()
-            }
+            const params = [
+              data.currentTitle,
+              data.newTitle,
+              data.salary,
+              data.departmentId,
+              data.id,
+            ];
+            db.query(sql, params, (err, result) => {
+              if (err) {
+                console.log(err);
+                return;
+              } else {
+                console.table(result);
+                init();
+              }
+            });
           });
-        });
       } else {
         console.log("Invalid choice!");
       }
     });
-
 
   // db.query(sql, params, (err, result) => {
   //     if(err) {
